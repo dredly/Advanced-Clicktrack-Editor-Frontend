3b233557c4594ad50816546095410092f0f71eb8
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-05-30 14:44:10 +0100

    Basic ToneJS setup with audio sample on cloudinary

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import * as Tone from 'tone'
+import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { displayForm } from './reducers/sectionReducer'
 import NewSection from './components/NewSection'
@@ -5,8 +7,29 @@ import NewSection from './components/NewSection'
 
 const App = () => {
 	const dispatch = useDispatch()
+	const [started, setStarted] = useState(false)
 	const sections = useSelector(state => state.sections.sectionList)
 	const formLocation = useSelector(state => state.sections.formLocation)
+
+	const woodblock1 = new Tone
+		.Player('https://res.cloudinary.com/doemj9gq6/video/upload/v1651427128/Samples/Woodblock_oogia1.wav')
+		.toDestination()
+
+	const woodblock2 = new Tone
+		.Player('https://res.cloudinary.com/doemj9gq6/video/upload/v1651427128/Samples/Woodblock_oogia1.wav')
+		.toDestination()
+
+	woodblock2.volume.value = -8
+
+	const playClicktrack = () => {
+		if (!started) {
+			console.log('Starting')
+			Tone.start()
+			setStarted(true)
+		}
+		woodblock1.start(Tone.now())
+		woodblock2.start(Tone.now() + 1)
+	}
 
 	const showFormHere = location => {
 		dispatch(displayForm(location))
@@ -39,6 +62,7 @@ const App = () => {
 					}
 				</div>
 			)}
+			<button onClick={playClicktrack}>Play</button>
 		</>
 	)
 }

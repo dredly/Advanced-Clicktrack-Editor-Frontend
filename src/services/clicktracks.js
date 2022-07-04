bf7202b570bf189ff86d569326450c7adfbfcb8a
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-07-05 00:12:03 +0100

    Request is now sent to deployed backend

--- src/services/clicktracks.js
+++ src/services/clicktracks.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://127.0.0.1:5000'
+//const baseUrl = 'http://127.0.0.1:5000'
+const baseUrl = 'https://stunning-yosemite-61675.herokuapp.com'
 
 const sendData = async (data) => {
 	const response = await axios.post(`${baseUrl}/api/makemidi`, data)

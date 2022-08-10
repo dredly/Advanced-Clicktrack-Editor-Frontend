5fdaf77e24ab5a1942560999a51e7718f77f4884
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-08-10 16:18:40 +0100

    Can play polyrhythms with one instrument again

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,20 +1,22 @@
 import * as Tone from 'tone'
 
 import { useSelector } from 'react-redux'
-import { getClickTimesNonPoly } from '../utils/clickTimesCalculator'
+import { getClickTimesNonPoly, getClickTimesPoly } from '../utils/clickTimesCalculator'
 
 const Controls = () => {
 	const sections = useSelector(state => state.sections.sectionList)
 	const selectedSamples = useSelector(state => state.samples.samples)
 
+	// eslint-disable-next-line no-unused-vars
 	const clickTimesNonPoly = getClickTimesNonPoly(sections)
+	const clickTimesPoly = getClickTimesPoly(sections)
 
 	const strongPlayer = new Tone.Player(selectedSamples[0].strong.url).toDestination()
 	const weakPlayer = new Tone.Player(selectedSamples[0].weak.url).toDestination()
 
 	const playClickTrack = () => {
 		Tone.start()
-		clickTimesNonPoly.map(ct => {
+		clickTimesPoly.map(ct => {
 			return { ...ct, time: ct.time + Tone.now() }
 		}).forEach(click => {
 			if (click.downBeat) {

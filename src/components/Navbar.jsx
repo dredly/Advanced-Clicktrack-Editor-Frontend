eb1a10e516ac5c1b6aea56c70b60395dd192d13e
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-08-14 17:57:49 +0100

    Setup AppBar with toggle help option on it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleHelp } from '../reducers/uiReducer'
+
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
+import Switch from '@mui/material/Switch'
+import { FormControlLabel } from '@mui/material'
 import { Typography } from '@mui/material'
 
 const Navbar = () => {
+	const dispatch = useDispatch()
+	const showHelp = useSelector(state => state.ui.showHelp)
+
 	return (
 		<AppBar>
 			<Toolbar>
@@ -10,10 +18,22 @@ const Navbar = () => {
 					variant="h6"
 					noWrap
 					component="div"
-					sx={{ flexGrow: 1, display: { sm: 'block' } }}
+					sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
 				>
-            Advanced Click Track Editor
+					Advanced Click Track Editor
 				</Typography>
+				<FormControlLabel
+					control={
+						<Switch
+							checked={showHelp}
+							aria-label="login switch"
+							onChange={() => dispatch(toggleHelp())}
+							color="secondary"
+						/>
+					}
+					label='Toggle Help'
+				/>
+
 			</Toolbar>
 		</AppBar>
 	)

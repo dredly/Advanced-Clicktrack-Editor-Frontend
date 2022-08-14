acd8a403500490289d7470f389e5349937b809ea
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-08-14 11:57:33 +0100

    Added dynamically changing backend url

--- src/services/clicktracks.js
+++ src/services/clicktracks.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-// const baseUrl = 'http://127.0.0.1:5000'
-const baseUrl = 'https://clicktrack-audio-backend.herokuapp.com/'
+const baseUrl =  window.location.href.includes('clicktrack-redux')
+	? 'https://clicktrack-audio-backend.herokuapp.com/'
+	: 'http://127.0.0.1:5000'
 
 // Sends a trivial request to the backend to prevent heroku from
 // cold starting once a user requests a wav file

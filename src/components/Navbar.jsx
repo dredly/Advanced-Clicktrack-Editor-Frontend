9498c81e318a463b2607b8ec6e9f851805032d7a
Daniel Redly <dredly@gmail.com>
2022-08-31 09:16:21 +0100

    Restyled Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { toggleHelp } from '../reducers/uiReducer'
+import NavbarMenu from './NavbarMenu'
+import { setCurrentlyEditing } from '../reducers/userReducer'
+import { setSections } from '../reducers/sectionReducer'
 
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Switch from '@mui/material/Switch'
-import { FormControlLabel, Typography, IconButton } from '@mui/material'
+import { FormControlLabel, Typography, IconButton, Box } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
-import NavbarMenu from './NavbarMenu'
-import { setCurrentlyEditing } from '../reducers/userReducer'
-import { setSections } from '../reducers/sectionReducer'
 
 const Navbar = () => {
 	const dispatch = useDispatch()
@@ -37,30 +37,89 @@ const Navbar = () => {
 
 	return (
 		<AppBar position="relative">
-			<Toolbar>
-				<IconButton
-					size="large"
-					edge="start"
-					color="inherit"
-					aria-label="menu"
-					aria-controls="menu-appbar"
-					aria-haspopup="true"
-					sx={{ mr: 2 }}
-					onClick={handleMenu}
-				>
-					<MenuIcon />
-				</IconButton>
-				<NavbarMenu anchorEl={anchorEl} handleClose={handleClose} handleHome={handleHome}/>
-				<Typography
-					variant="h6"
-					noWrap
-					onClick={handleHome}
-					component={Link}
-					to="/"
-					sx={{ flexGrow: 1, color: 'white', textDecoration: 'none', display: { xs: 'none', sm: 'block' } }}
-				>
+			<Toolbar sx={{ justifyContent: 'space-between' }}>
+				<Box sx={{ display: 'flex', alignItems: 'center' }}>
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						aria-controls="menu-appbar"
+						aria-haspopup="true"
+						sx={{ mr: 2, display: { md: 'none' } }}
+						onClick={handleMenu}
+					>
+						<MenuIcon />
+					</IconButton>
+					<NavbarMenu anchorEl={anchorEl} handleClose={handleClose} handleHome={handleHome}/>
+					<Typography
+						variant="h6"
+						noWrap
+						onClick={handleHome}
+						component={Link}
+						to="/"
+						sx={{ flexGrow: 1, color: 'white', textDecoration: 'none', display: { xs: 'none', sm: 'block' } }}
+					>
 					Advanced Click Track Editor
-				</Typography>
+					</Typography>
+					{user
+						?
+						<>
+							<Typography
+								variant = 'subtitle1'
+								sx={{
+									flexGrow: 1,
+									color: 'white',
+									textDecoration: 'none',
+									ml: 5,
+									display: { xs: 'none', md: 'block' }
+								}}
+							>
+								My clicktracks
+							</Typography>
+							<Typography
+								variant = 'subtitle1'
+								sx={{
+									flexGrow: 1,
+									color: 'white',
+									textDecoration: 'none',
+									ml: 5,
+									display: { xs: 'none', md: 'block' }
+								}}
+							>
+								Logout
+							</Typography>
+						</>
+
+						:
+						<>
+							<Typography
+								variant = 'subtitle1'
+								sx={{
+									flexGrow: 1,
+									color: 'white',
+									textDecoration: 'none',
+									ml: 5,
+									display: { xs: 'none', md: 'block' }
+								}}
+							>
+								Login
+							</Typography>
+							<Typography
+								variant = 'subtitle1'
+								sx={{
+									flexGrow: 1,
+									color: 'white',
+									textDecoration: 'none',
+									ml: 5,
+									display: { xs: 'none', md: 'block' }
+								}}
+							>
+								Register
+							</Typography>
+						</>
+					}
+				</Box>
 				<FormControlLabel
 					control={
 						<Switch

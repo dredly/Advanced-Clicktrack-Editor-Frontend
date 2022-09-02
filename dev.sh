bda47356a9e499030530e98c897c4049cc82c157
Daniel Redly <dredly@gmail.com>
2022-09-02 15:04:17 +0100

    Created shell scripts to simplify starting the dev and end to end testing environments

---
+++ dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gnome-terminal \
+    --tab --title="Audio Backend" --command="bash -c 'cd ../Advanced-Clicktrack-Audio-Backend; ./rebuild.sh; $SHELL'" \
+    --tab --title="User Backend" --command="bash -c 'cd ../Advanced-Clicktrack-User-Backend; npm run dev; $SHELL'" \
+    --tab --title="Frontend" --command="bash -c 'npm start; $SHELL'" \

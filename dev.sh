2b114de26816f8c36a8d7f4b95ed1fd9e2071367
Daniel Redly <dredly@gmail.com>
2022-09-02 15:16:56 +0100

    Updated shell scripts to not use deprecated --command option

--- dev.sh
+++ dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 gnome-terminal \
-    --tab --title="Audio Backend" --command="bash -c 'cd ../Advanced-Clicktrack-Audio-Backend; ./rebuild.sh; $SHELL'" \
-    --tab --title="User Backend" --command="bash -c 'cd ../Advanced-Clicktrack-User-Backend; npm run dev; $SHELL'" \
-    --tab --title="Frontend" --command="bash -c 'npm start; $SHELL'" \
+    --tab --title="Audio Backend" -- bash -c "cd ../Advanced-Clicktrack-Audio-Backend; ./rebuild.sh; $SHELL"
+gnome-terminal \
+    --tab --title="User Backend" -- bash -c "cd ../Advanced-Clicktrack-User-Backend; npm run dev; $SHELL"
+gnome-terminal \
+    --tab --title="Frontend" -- bash -c "npm start; $SHELL" \
--- e2e.sh
+++ e2e.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 gnome-terminal \
-    --tab --title="Audio Backend" --command="bash -c 'cd ../Advanced-Clicktrack-Audio-Backend; ./rebuild.sh; $SHELL'" \
-    --tab --title="User Backend" --command="bash -c 'cd ../Advanced-Clicktrack-User-Backend; npm run dev; $SHELL'" \
-    --tab --title="Frontend" --command="bash -c 'npm start; $SHELL'" \
-    --tab --title="Cypress" --command="bash -c 'cypress open; $SHELL'" \
+    --tab --title="Audio Backend" -- bash -c "cd ../Advanced-Clicktrack-Audio-Backend; ./rebuild.sh; $SHELL"
+gnome-terminal \
+    --tab --title="User Backend" -- bash -c "cd ../Advanced-Clicktrack-User-Backend; npm run dev; $SHELL"
+gnome-terminal \
+    --tab --title="Frontend" -- bash -c "npm start; $SHELL"
+gnome-terminal \
+    --tab --title="Cypress" -- bash -c "cypress open; $SHELL"
